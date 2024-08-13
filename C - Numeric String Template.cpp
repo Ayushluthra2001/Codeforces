f68cafe1850af3e1322f51c4e1e8a593b964a68c
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-08-13 22:50:53 +0530

    Create C - Numeric String Template.cpp

---
+++ C - Numeric String Template.cpp
@@ -0,0 +1,42 @@
+By ayushluthra2001, contest: Codeforces Round 966 (Div. 3), problem: (A) Primary Task, Accepted, #, Copy, hack it!
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+ 
+ 
+    while (t--) {
+        int n;
+        cin>>n;
+        string s = to_string(n);
+ 
+        if(s.length()<=2){
+            cout<<"NO"<<endl;
+        }else{
+ 
+            if(s[0]=='1' && s[1]=='0'){
+                int x = 0;
+                for (int i=2;i<s.length();i++) {
+                        x=x*10+(s[i]-'0');
+                    }
+                if(x >=2 && s[2]!='0') cout<<"YES"<<endl;
+                else cout<<"NO"<<endl;
+ 
+ 
+            }else{
+                cout<<"NO"<<endl;
+            }
+ 
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+    return 0;
+}
